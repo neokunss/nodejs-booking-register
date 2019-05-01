fee28312009bccff5e50b7216c3e7fc3a54926a5
Kun Srithaporn <kn.srithaporn@gmail.com>
2019-05-01 15:57:40 +0700

    finshed flow design

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var session = require('express-session');
-var flash = require('connect-flash');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 
 var mongoose = require('mongoose');
@@ -15,8 +15,10 @@ mongoose.connect('mongodb://localhost/dbusers');
 require("./models/User");
 
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+let index = require('./routes/index');
+let register = require('./routes/register');
+// var register_profile = require('./routes/register-profile');
+let users = require('./routes/users');
 
 var app = express();
 
@@ -25,7 +27,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,6 +35,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/register', register);
+// app.use('/register-profile', register_profile);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
