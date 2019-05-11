5f04165e938365ae3f3ac4fa2ab8b805c55aa5c0
Kun Srithaporn <kn.srithaporn@gmail.com>
2019-05-12 04:38:58 +0700

    wwww

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
-
+const env = require("dotenv").config();
+console.log(env.parsed);
 const express = require("express");
 const path = require("path");
 const favicon = require("serve-favicon");
