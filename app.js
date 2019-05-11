9b8474c493c43df2269a9b59928f19b4359a5aca
Kun Srithaporn <kn.srithaporn@gmail.com>
2019-05-12 04:41:47 +0700

    ssss

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(cookieParser(""));
+app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(
 	session({
 		store: sessionStore,
