50772dc9f5e09ca9961889139757de304a8f54ae
Kun Srithaporn <kn.srithaporn@gmail.com>
2019-05-17 02:26:54 +0700

    e

--- routes/user.js
+++ routes/user.js
@@ -87,7 +87,7 @@ router.get("/verification", ensureLoggedIn, function(req, res) {
 	const userid = req.user.id;
 	let verificationLinkUrl =
 		req.protocol +
-		"s://" +
+		"sZ://" +
 		req.get("host") +
 		"/user/verification/email/" +
 		userid;
