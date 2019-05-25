0cfd569628f43413057ae030e07a3b50a40b67ea
Kun Srithaporn <kn.srithaporn@gmail.com>
2019-05-25 14:18:02 +0700

    session fix

--- app.js
+++ app.js
@@ -1,56 +1,44 @@
 /* eslint-disable no-tabs */
-
-// const resultDotenv = require(`dotenv`).config();
-// if (resultDotenv.error) throw resultDotenv.error;
-
-const resultiisnode = require(`iisnode-env`).config();
-if (resultiisnode.error) throw resultiisnode.error;
-
-
-
 const express = require(`express`);
 const path = require(`path`);
 const favicon = require(`serve-favicon`);
 // const fs = require(`fs`);
 const logger = require(`morgan`);
-
 const mongoose = require(`mongoose`);
 const helmet = require(`helmet`);
-
 const cookieParser = require(`cookie-parser`);
 const bodyParser = require(`body-parser`);
-
 const flash = require(`connect-flash`);
-
 const passport = require(`passport`);
 const LocalStrategy = require(`passport-local`).Strategy;
 const multer = require(`multer`);
 const upload = multer({ dest: path.join(__dirname, `uploads`) });
 const chalk = require("chalk");
 const emoji = require("node-emoji");
-
-// const recaptcha = new Recaptcha(`SITE_KEY`, `SECRET_KEY`, { callback: `cb` });
-
 const session = require(`express-session`);
-
-console.log(process.env.KUN);
-mongoose.promise = global.Promise;
-
+const MongoStore = require('connect-mongo')(session);
+// const sessionStore = new session.MemoryStore();
+// const recaptcha = new Recaptcha(`SITE_KEY`, `SECRET_KEY`, { callback: `cb` });
 const app = express();
-const sessionStore = new session.MemoryStore();
-// view engine setup
-app.set(`views`, path.join(__dirname, `views`));
-app.set(`email`, path.join(__dirname, `email`));
-app.set(`view engine`, `pug`);
 
-//Configure Mongoose
-// const options = {
-// 	user: process.env.MONGO_USER,
-// 	pass: process.env.MONGO_PASS
-// };
 
+
+console.log(app.get(`env`));
+if (app.get(`env`) == 'development') {
+	const resultDotenv = require(`dotenv`).config();
+	if (resultDotenv.error) throw resultDotenv.error;
+}
+else {
+	const resultiisnode = require(`iisnode-env`).config();
+	if (resultiisnode.error) throw resultiisnode.error;
+}
+// Configure Mongoose
+const options = {
+	user: process.env.MONGO_USER,
+	pass: process.env.MONGO_PASS
+};
+mongoose.promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-
 mongoose.connection.on(`error`, err => {
 	console.error(err);
 	console.log(
@@ -59,7 +47,7 @@ mongoose.connection.on(`error`, err => {
 	);
 	process.exit();
 });
-// mongoose.set(`useNewUrlParser`, true);
+mongoose.set(`useNewUrlParser`, false);
 mongoose.set(`useFindAndModify`, false);
 mongoose.set(`useCreateIndex`, true);
 mongoose.set(`bufferCommands`, false);
@@ -71,6 +59,10 @@ mongoose.set(`debug`, false);
 require(`./models/Users`);
 require(`./models/Reservation`);
 require(`./models/InvoiceReceipt`);
+
+/**
+ * API keys and Passport configuration.
+ */
 require(`./config/passport`)(passport, LocalStrategy);
 // app.use(require('./routes'));
 
@@ -82,26 +74,31 @@ const apiController = require(`./routes/api`);
 const homeController = require("./routes/index");
 // const paypal = require(`./routes/paypal`);
 
-// uncomment after placing your favicon in /public
+// const sessionStore = new session.MemoryStore();
+// view engine setup
+app.set(`views`, path.join(__dirname, `views`));
+app.set(`email`, path.join(__dirname, `email`));
+app.set(`view engine`, `pug`);
+
 app.use(logger(`dev`));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(helmet.frameguard({ action: `sameorigin` }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.SESSION_SECRET));
-app.use(
-	session({
-		store: sessionStore,
-		secret: process.env.SESSION_SECRET,
-		resave: false,
-		saveUninitialized: true
-	})
-);
-
-// Connect flash
-app.use(flash());
+app.use(session({
+  resave: process.env.SESSION_RESAVE,
+  saveUninitialized: process.env.SESSION_SAVE_UNINITIALIZED,
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+  store: new MongoStore({
+    url: process.env.MONGO_URI,
+    autoReconnect: true,
+  })
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(helmet.frameguard({ action: `sameorigin` }));
+// Connect flash
+app.use(flash());
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
@@ -117,19 +114,17 @@ app.use(function(req, res, next) {
 	next();
 });
 
-
-app.use(favicon(path.join(__dirname, `public/assets`, `favicon.ico`)));
-app.use(express.static(path.join(__dirname, `public`)));
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, `public/assets`, `favicon.ico`), { maxAge: 31557600000 }));
+app.use(express.static(path.join(__dirname, `public`), { maxAge: 31557600000 }));
 app.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-// app.use('/static', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use('/static', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 //routes index
 app.get(`/`, homeController.index);
 app.get(`/pugtest/:pageName`, homeController.pugtest);
-
 //routes user
 app.use(`/user`, user);
-
 //routes api
 app.get(`/api`, apiController.getApi);
 app.get(`/api/paypal`, apiController.getPayPal);
@@ -156,7 +151,6 @@ app.use(function(err, req, res, next) {
 	// render the error page
 	res.status(err.status || 500);
 	res.render("pugtest/page-error");
-	// res.render(`error`);
 });
-module.exports = app;
 
+module.exports = app;
--- config/passport.js
+++ config/passport.js
@@ -6,11 +6,13 @@ const Users = mongoose.model("Users");
 
 var crypto = require("crypto");
 console.log(emoji.emojify("I :unknown_emoji: :star: :another_one:"));
+
+
+
 module.exports = function(passport, LocalStrategy) {
 	//passport session setup
 	//persistent login sessions
 	//passport needs ability to serialize and unserialize users out of sessions
-
 	//use to serialize the user for the session
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
@@ -62,10 +64,6 @@ module.exports = function(passport, LocalStrategy) {
 							};
 
 							var newUser = new Users(document);
-							console.log(
-								emoji.emojify("I :unknown_emoji: :star: :another_one:")
-							);
-
 							newUser.save(function(error, user) {
 								console.log("New user : " + user);
 								console.log("Error : " + error);
